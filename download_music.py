b2f97fd5e31e4ed49f24dc2bef043c15a29ad4a0
Smoodie <89829273+Smoodie7@users.noreply.github.com>
2023-05-28 20:50:51 +0100

    Update download_music.py

--- download_music.py
+++ download_music.py
@@ -1,44 +1,115 @@
 import subprocess
 import os
 import sys
+import shutil
+import logging
 
 
 def download_song(query):
+    # Create a hidden temp directory and delete if exists
+    temp_dir = os.path.join(os.getcwd(), '.temp')
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+    os.mkdir(temp_dir)
+
+    def process_query(query_item):
+        query_item = query_item.replace("'", " ").replace('"', ' ')
+        if query_item.startswith("https"):
+            download_command = f'spotdl "{query_item}" --output {os.path.abspath(".temp")}'
+        else:
+            download_command = f'spotdl \'{query_item}\' --output {os.path.abspath(".temp")}'
+
+        # Execute the command in the shell
+        logging.info(f"Executing: {download_command}")
+        subprocess.run(download_command, shell=True)
+
+        if sys.platform == 'darwin':
+            add_to_music_app_and_sync(os.path.abspath(".temp"))
+
+        # Move the downloaded file to Musics folder
+        for filename in os.listdir(temp_dir):
+            shutil.move(os.path.join(temp_dir, filename), os.path.join(os.getcwd(), "Musics", filename))
 
     if isinstance(query, list):
         len_query = len(query)
-        for i in range(len_query):
-            query = query[i].replace("'", " ").replace('"', ' ')
-            print(f"{i+1}/{len_query}")
-            download_command = f'spotdl "{query[i]}" --output {os.path.abspath("Musics")}'
-
-            # Execute the command in the shell
-            subprocess.run(download_command, shell=True)
+        for i, query_item in enumerate(query, start=1):
+            print(f"{i}/{len_query}")
+            process_query(query_item)
     else:
-        query = query.replace("'", " ").replace('"', ' ')
-        if query.startswith("https"):
-            download_command = f'spotdl "{query}" --output {os.path.abspath("Musics")}'
+        process_query(query)
+
+    # Delete the temp directory
+    shutil.rmtree(temp_dir)
+
+
+def add_to_music_app_and_sync(temp_folder_path):
+    # Iterate over all files in the given folder
+    for filename in os.listdir(temp_folder_path):
+        # Check if the file is a music file
+        if filename.endswith(".mp3") or filename.endswith(".wav") or filename.endswith(".flac"):
+            # If it is, add it to the Music app
+            music_file_path = os.path.join(temp_folder_path, filename)
+            logging.info(f"Transferring {music_file_path} to Music App..")
+
+            # Use AppleScript to add the song to the Music App Library
+            applescript = f"""
+                tell application "Music"
+                    add POSIX file "{music_file_path}"
+                end tell
+                """
+            subprocess.call(["osascript", "-e", applescript])
+
+    # Get device name
+    try:
+        print("Trying to find devices..")
+        result = subprocess.run(["osascript", "-e",
+                                 """
+                                 do shell script "system_profiler SPUSBDataType | awk -F': ' '/iPhone|iPad/{getline; print $2}'"
+                                 """
+                                 ], capture_output=True, text=True, timeout=5)
+        if result.returncode == 0:
+            device_name = result.stdout.strip()
         else:
-            download_command = f'spotdl \'{query}\' --output {os.path.abspath("Musics")}'
+            device_name = None
 
-        # Execute the command in the shell
-        subprocess.run(download_command, shell=True)
+        if not device_name:
+            print("Didn't found connected device, trying wireless..")
+            # Attempt to get the name of a wireless connected device
+            # Here we are running a shell script that will timeout after 5 seconds
+            result = subprocess.run(['sh', '-c',
+                                     """
+                                     timeout 5 dns-sd -B _raop._tcp local | grep 'iPhone' | head -1 | awk '{print $7}' | cut -d '.' -f 1
+                                     """
+                                     ], capture_output=True, text=True)
+            if result.returncode == 0:
+                device_name = result.stdout.strip()
 
-#TO DO: if sys.platform == 'darwin':
-#            add_to_music_app_and_delete(music_file_path)
+        if device_name:
+            print(f"Found {device_name}, trying to sync Music Library with..")
+        else:
+            logging.error('Failed to retrieve connected device name')
+            return
+    except subprocess.TimeoutExpired:
+        logging.error('Failed to retrieve connected device name')
+        return
 
-# Add the music to Music App then delete
-def add_to_music_app_and_delete(music_file_path):
-    # Use AppleScript to add the song to the Music App Library
-    applescript = f"""
-    tell application "Music"
-        add POSIX file "{music_file_path}"
-    end tell
-    """
-    subprocess.call(["osascript", "-e", applescript])
-
-    # Use os module to delete the music file
-    os.remove(music_file_path)
+    # If device is found, attempt to sync
+    if device_name:
+        applescript_sync = f"""
+            tell application "System Events"
+                tell process "Finder"
+                    set frontmost to true
+                    click menu item "Sync {device_name}" of menu 1 of menu bar item "File" of menu bar 1
+                end tell
+            end tell
+            """
+        try:
+            subprocess.call(["osascript", "-e", applescript_sync])
+            print(f"Synchronized with {device_name} successfully!")
+            logging.info(f"Synchronized with {device_name} successfully!")
+        except subprocess.CalledProcessError:
+            print(f'Failed to sync with device: {device_name}')
+            logging.error(f'Failed to sync with device: {device_name}')
 
 
 # DO NOT PAY ATTENTION HERE
